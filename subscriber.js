152603e07ba0aed195e8df812f88855213e2d4ba
Omkar Patkar <40171054+OmkarPatkar@users.noreply.github.com>
2023-06-06 14:58:37 +0530

    added one more subscriber

--- subscriber.js
+++ subscriber.js
@@ -5,22 +5,36 @@ const redisClient = new Redis();
 
 // check if redis is connected and running
 subscriber.on('connect', function() {
-  console.log('Connected!'); // Connected!
+console.log('Connected!'); // Connected!
 });
 
 // create channels to subscribe to
-
-const channel1 = 'status1';
+const channel1 = 'chat1';
+const channel2 = 'chat2'; // Add additional channel
 
 // subscribe to channels
 redisClient.subscribe(channel1, (err, channel) => {
 	if (err) {
 		throw new Error(err);
 	}
-	console.log(`subscribed to ${channel1} channel. Listening for updates on the ${channel1} channel...`);
+	console.log(`Subscribed to ${channel1} channel. Listening for updates on the ${channel1} channel...`);
+
+});
+
+redisClient.subscribe(channel2, (err, channel) => {
+	if (err) {
+		throw new Error(err);
+	}
+	console.log(`Subscribed to ${channel2} channel. Listening for updates on the ${channel2} channel...`);
+
 });
 
 // Receive message from channels
 redisClient.on('message', (channel, message) => {
-	console.log(`Received message from ${channel1} channel: ${message}`);
+	if (channel === channel1) {
+		console.log(`Received message from ${channel1} channel: ${message}`);
+	} else if (channel === channel2) {
+		console.log(`Received message from ${channel2} channel: ${message}`);
+}
 });
+
